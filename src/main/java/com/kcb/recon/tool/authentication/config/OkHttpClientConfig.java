e33d9731ba69f513743c168580b220ca9a547695
Kenson Kimutai <kensonlangat1@gmail.com>
2025-03-12 10:38:02 +0300

    updated overall overhaul of user-management and role -matrix management changes

---
+++ src/main/java/com/kcb/recon/tool/authentication/config/OkHttpClientConfig.java
@@ -0,0 +1,31 @@
+package com.kcb.recon.tool.authentication.config;
+
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class OkHttpClientConfig {
+
+
+    @Value("${http-connection-timeout}")
+    private int connectionConnectTimeout;
+
+    @Value("${http-connection-read-timeout}")
+    private int connectionReadTimeout;
+
+    @Value("${http-connection-write-timeout}")
+    private int connectionWriteTimeout;
+
+    @Bean
+    public OkHttpClient createOkHttpClient() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(connectionConnectTimeout, TimeUnit.SECONDS)
+                .readTimeout(connectionReadTimeout, TimeUnit.SECONDS)
+                .writeTimeout(connectionWriteTimeout, TimeUnit.SECONDS)
+                .build();
+    }
+}
